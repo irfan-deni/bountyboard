c3489c9b9b7914c7dfb361eca4d4a6f2d0eff6cf
zieqs <hazieqfarhann@gmail.com>
2026-06-26 03:33:04 +0800

    fixed the login, register, and logout into a work-ish state

--- core/settings.py
+++ core/settings.py
@@ -125,5 +125,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/accounts/login/'
+LOGOUT_REDIRECT_URL = 'home'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
